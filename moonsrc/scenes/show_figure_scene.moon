2efeee6168775a1524f5e8434f5175a8143880a5
yi <yi2004@gmail.com>
2014-07-27 03:57:39 +0800

    working on figure

--- moonsrc/scenes/show_figure_scene.moon
+++ moonsrc/scenes/show_figure_scene.moon
@@ -12,7 +12,7 @@ create = (gamaFigure) ->
   assert gamaFigure, "missing figure instance to play on."
 
   xpos = display.cx
-  ypos = display.cy
+  ypos = display.height / 3
 
   gamaFigure\setDefaultMotion "idl"
   gamaFigure\setDefaultDirection "s"
@@ -29,6 +29,26 @@ create = (gamaFigure) ->
   line\drawSegment(cc.p(0, ypos), cc.p(display.width, ypos), 0.5, borderColor)
   line\drawSegment(cc.p(xpos, 0), cc.p(xpos, display.height), 0.5, borderColor)
   scene\addChild line
+
+  motions = gamaFigure\getMotions!
+
+  -- add menu items for tests
+  LINE_SPACE = 40
+  s = cc.Director\getInstance()\getWinSize()
+  MainMenu = cc.Menu\create!
+  for index, motion in ipairs motions
+    testLabel = cc.Label\create(motion, 'Arial', 24)
+    testLabel\setAnchorPoint(cc.p(0.5, 0.5))
+    testMenuItem = cc.MenuItemLabel\create testLabel
+    testMenuItem\registerScriptTapHandler ->
+      gamaFigure\playOnSprite sprite, motion
+      return
+    testMenuItem\setPosition(cc.p(100 / 2, (s.height - (index) * LINE_SPACE)))
+    MainMenu\addChild(testMenuItem, index + 10000, index + 10000)
+
+  MainMenu\setContentSize(cc.size(100, (#motions + 1) * (LINE_SPACE)))
+  MainMenu\setPosition(0, 0)
+  scene\addChild(MainMenu)
 
   return scene
 
--- src/scenes/show_figure_scene.lua
+++ src/scenes/show_figure_scene.lua
@@ -6,7 +6,7 @@ create = function(gamaFigure)
   scene = cc.Scene:create()
   assert(gamaFigure, "missing figure instance to play on.")
   local xpos = display.cx
-  local ypos = display.cy
+  local ypos = display.height / 3
   gamaFigure:setDefaultMotion("idl")
   gamaFigure:setDefaultDirection("s")
   local sprite = cc.Sprite:create()
@@ -18,6 +18,23 @@ create = function(gamaFigure)
   line:drawSegment(cc.p(0, ypos), cc.p(display.width, ypos), 0.5, borderColor)
   line:drawSegment(cc.p(xpos, 0), cc.p(xpos, display.height), 0.5, borderColor)
   scene:addChild(line)
+  local motions = gamaFigure:getMotions()
+  local LINE_SPACE = 40
+  local s = cc.Director:getInstance():getWinSize()
+  local MainMenu = cc.Menu:create()
+  for index, motion in ipairs(motions) do
+    local testLabel = cc.Label:create(motion, 'Arial', 24)
+    testLabel:setAnchorPoint(cc.p(0.5, 0.5))
+    local testMenuItem = cc.MenuItemLabel:create(testLabel)
+    testMenuItem:registerScriptTapHandler(function()
+      gamaFigure:playOnSprite(sprite, motion)
+    end)
+    testMenuItem:setPosition(cc.p(100 / 2, (s.height - (index) * LINE_SPACE)))
+    MainMenu:addChild(testMenuItem, index + 10000, index + 10000)
+  end
+  MainMenu:setContentSize(cc.size(100, (#motions + 1) * (LINE_SPACE)))
+  MainMenu:setPosition(0, 0)
+  scene:addChild(MainMenu)
   return scene
 end
 return {
